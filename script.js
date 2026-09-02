0039644656b6aa46239c5c3823aeb893d38c3858
Swethakadiri <kadiriswetha755@gmail.com>
2026-09-02 16:08:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,94 +1,97 @@
-let a ={
-    title:"pfsd",
-    description:"full stack",
-    trainer:"swetha"
-}
+const a=document.getElementById("totalIncome");
+const b=document.getElementById("totalExpenses");
+const c=document.getElementById("totalBalance");
+const d=document.getElementById("form");
+const k=document.getElementById("name");
+const f=document.getElementById("amount");
+const g=document.getElementById("transactionType");
+const h=document.getElementById("transactionCategory");
+const i=document.getElementById("list");
+const j=document.getElementById("message");
 
-console.log(a['title']);
+let transactions=[];
 
+let savedTransactions=localStorage.getItem("transactions");
 
-//reference
-let k={name:"naresh"};
-let y=k;
-k="swetha";
-console.log(k);
-console.log(y);
-
-///Arrays
-let course=['pfsd','jfsd','mern'];
-console.log(course);
-console.log(course[0]);
-console.log(course[1]);
-console.log(course[2]);
-console.log(course[3]);
-console.log(course[4]);
-console.log(course[5]);
-
-//execution content
-//1.memory phase-variable environment
-//2.code phase-thread of  environment
-createCourse('jfsd');
-console.log(m)
-function createCourse(coursename){
-    console.log('creating' + coursename);
-}
-createCourse('pfsd');
-createCourse('jfsd');
-
-
-//let & var ,const
-{
-let z = 10;
-var o = 20;
-const v = 30;
-}
-console.log(z);
-console.log(o);
-console.log(v);
-
-//
-function hello(){
-    var x = 10;
-    console.log(x);
+if(savedTransactions){
+    transactions=JSON.parse(savedTransactions);
 
 }
-hello();
+d.addEventListener("submit",addTransaction);
+function addTransaction(event){
+    event.preventDefault();
+    const name = k.value.trim();
+    const amount = Number(f.value);
+    const category = h.value;
+    const type = g.value;
+    if(name === "" || amount <=0){
+        j.textContent = "enter a valid name and amount";
+        return;
+    };
+    const transaction = {
+        id : Date.now(),
+        name:name,
+        amount:amount,
+        category:category,
+        type:type
 
-//add
-let m = function add(a,b){
-    return a+b
+    };
+    transactions.push(transaction);
+    saveTransactions();
+    renderTransactions();
+    d.reset();
+    j.textContent="Transaction added successfully";
+};
+
+
+
+
+
+
+function renderTransactions(transactionArray = transactions){
+    i.innerHTML ="";
+    transactionArray.forEach(function(transaction){
+        const sign = transaction.type === "income" ? "+" : "-";
+        const amountClass = transaction.type === "income" ? "income-amount" : "expense-amount";
+        i.innerHTML +=`
+        <div class = "transaction">
+            <div class="transaction-info">
+            <h3>${transaction.name}</h3>
+            <p>${transaction.category}</p>
+            </div>
+            <div class="transaction-amount">
+            <span class="${amountClass}">
+            ${sign}${transaction.amount}
+            </span>
+            </div>
+        </div>
+        `;
+       }
+);
+calculateTotals();
+};
+function calculateTotals(){
+    const income = transactions.filter(function(transaction){
+        return transaction.type === "income";})
+        .reduce(function(total,transaction){
+            return total + transaction.amount;
+    },0);
+    const expense = transactions.filter(function(transaction){
+        return transaction.type === "expense";})
+        .reduce(function(total,transaction){
+            return total + transaction.amount;
+},0);
+
+
+const currentBalance = income - expense;
+a.textContent = income;
+b.textContent = expense;
+c.textContent = currentBalance;
 }
-console.log(a);
-console.log(a(1,2));
-
-//
-let add=(a,b) => a+b;
-let diff = (a,b) => a-b;
-console.log(add(2,3));
-console.log(diff(2,3));
-
-//
-let n = 10;
-function outer(){
-    n = 100;
-    function inner(){
-        console.log(n);
-    }
-    return inner;
-}
-let returnFunc = outer();
-console.log(returnFunc);
-returnFunc();
+function saveTransactions(){
+    localStorage.setItem("transactions",JSON.stringify(transactions));}
+    renderTransactions();
 
 
-
-
-
-
-
-
-
-
-
-
-
+                
+            
